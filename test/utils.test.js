f025c70eeb089f19efe74c43892b05ec918336f1
qiuhaoxin <502967244@qq.com>
2019-05-27 00:20:17 +0800

    commit

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,11 +2,19 @@
 
 import {isEmpty} from '../src/utils/index.js';
 
-const assert=require('../src/utils/index.js');
+const assert=require('assert');
 
 
 describe('utility',function(){
 	it('isEmpty()',function(){
 		assert.equal(isEmpty('dd'),false);
+	})
+
+	it('shallow equal',function(){
+		assert.equal({a:{b:1}},{a:{b:1}},`出错啦！`);
+	})
+
+	it('ok',function(){
+		assert(false);
 	})
 })
